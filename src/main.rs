2aeb71158c056a20eb7050f46ccd43217ea2fa76
rmlaz <rnnlaz@outlook.com>
2026-08-29 10:44:09 +0800

    Refactor main function to support asynchronous operations; enhance WiFi connection handling and TCP socket management

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,45 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use esp_hal::{
-    clock::CpuClock, interrupt::software::SoftwareInterruptControl, ram, time::{Duration, Instant}, timer::timg::TimerGroup,
+use embassy_futures::select::Either;
+use embassy_time::{Duration, Timer};
+use embassy_executor::Spawner;
+use embassy_net::{
+    IpListenEndpoint,
+    Runner,
+    StackResources,
+    dns::DnsSocket,
+    tcp::{
+        TcpSocket,
+        client::{TcpClient, TcpClientState},
+    },
 };
 
-use esp_println::println;
+use esp_hal::{
+    clock::CpuClock,
+    ram,
+    timer::timg::TimerGroup,
+    rng::Rng,
+};
+
+use esp_println::{println, print};
+use esp_radio::wifi::{
+    AuthenticationMethodConfig, Config, ControllerConfig, Interface, WifiController, ap::AccessPointConfig, sta::StationConfig,
+};
 
 #[panic_handler]
-fn panic(info : &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
     println!("Panic!: {:?}", info);
     loop {}
+}
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
 }
 
 mod probe;
@@ -33,7 +62,7 @@ const PASSWORD: &str = env!("PASSWORD");
     reason = "it's not unusual to allocate larger buffers etc. in main"
 )]
 #[esp_hal::main]
-fn main() -> ! {
+async fn main(spawner: Spawner) -> ! {
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -42,13 +71,173 @@ fn main() -> ! {
     esp_alloc::heap_allocator!(size: 36 * 1024);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
+    esp_rtos::start(timg0.timer0, peripherals.FROM_CPU_INTR0);
 
+    let station_config = Config::Station(
+        StationConfig::default()
+            .with_ssid(SSID.try_into().unwrap())
+            .with_authentication(AuthenticationMethodConfig::Wpa2Personal(
+                PASSWORD.try_into().unwrap(),
+            )),
+    );
 
+    println!("linking wifi...");
+    let wifi_sta_device = esp_radio::wifi::Interface::station();
+    let controller = esp_radio::wifi::WifiController::new(
+        peripherals.WIFI,
+        ControllerConfig::default().with_initial_config(station_config),
+    )
+    .unwrap();
+
+    let sta_config = embassy_net::Config::dhcpv4(Default::default());
+
+    let rng = Rng::new();
+    let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+
+    let (sta_stack, sta_runner) = embassy_net::new(
+        wifi_sta_device,
+        sta_config,
+        mk_static!(StackResources<4>, StackResources::<4>::new()),
+        seed,
+    );
+
+    spawner.spawn(connection(controller).unwrap());
+    spawner.spawn(net_task(sta_runner).unwrap());
+
+    let sta_address = loop {
+        if let Some(config) = sta_stack.config_v4() {
+            let address = config.address.address();
+            println!("Got IP: {}", address);
+            break address;
+        }
+        println!("Waiting for IP...");
+        Timer::after(Duration::from_millis(500)).await;
+    };
+
+    println!("wifi linked!");
+
+    let tcp_client = TcpClient::new(
+        sta_stack,
+        mk_static!(
+            TcpClientState<1, 1500, 1500>,
+            TcpClientState::<1, 1500, 1500>::new()
+        ),
+    );
+    let dns_client = DnsSocket::new(sta_stack);
+
+    let mut sta_server_rx_buffer = [0; 1536];
+    let mut sta_server_tx_buffer = [0; 1536];
+
+    let mut sta_server_socket = TcpSocket::new(
+        sta_stack,
+        &mut sta_server_rx_buffer,
+        &mut sta_server_tx_buffer,
+    );
+    sta_server_socket.set_timeout(None);
 
     loop {
-        let start = Instant::now();
-        while start.elapsed() < Duration::from_secs(2) {}
+        println!("Wait for connection...");
+
+        let result = sta_server_socket.accept(IpListenEndpoint {
+            addr: None,
+            port: 8080,
+        }).await;
+
+        if let Err(e) = result {
+            println!("Failed to accept connection: {:?}", e);
+            continue;
+        }
+
+        println!("Client connected!");
+
+        let mut buffer = [0u8; 1024];
+        let mut pos = 0;
+        loop {
+            match sta_server_socket.read(&mut buffer).await {
+                Ok(0) => {
+                    println!("Read EOF");
+                    break;
+                }
+                Ok(len) => {
+                    pos += len;
+                    match core::str::from_utf8(&buffer[..pos]) {
+                        Ok(to_print) => {
+                            if to_print.contains("\r\n") {
+                                print!("Received: {}", to_print);
+                                use embedded_io_async::Write;
+
+                                let response = &buffer[..pos];
+                                if let Err(e) = sta_server_socket.write_all(response).await {
+                                    println!("Failed to send response: {:?}", e);
+                                }
+
+                                let _ = sta_server_socket.flush().await;
+                                continue;
+                            }
+                        }
+                        Err(e) => {
+                            println!("Failed to parse received data as UTF-8: {:?}", e);
+                            buffer = [0u8; 1024];
+                            pos = 0;
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("Failed to read from socket: {:?}", e);
+                    break;
+                }
+            }
+        }
+        sta_server_socket.abort();
     }
+}
+
+#[embassy_executor::task]
+async fn connection(mut controller: WifiController<'static>) {
+    println!("start connection task");
+
+    loop {
+        match controller.connect_async().await {
+            Ok(_) => {
+                // wait until we're no longer connected
+                loop {
+                    let info = embassy_futures::select::select(
+                        controller.wait_for_disconnect_async(),
+                        controller.wait_for_access_point_connected_event_async(),
+                    )
+                    .await;
+
+                    match info {
+                        Either::First(station_disconnected) => {
+                            if let Ok(station_disconnected) = station_disconnected {
+                                println!("Station disconnected: {:?}", station_disconnected);
+                                break;
+                            }
+                        }
+                        Either::Second(event) => {
+                            if let Ok(event) = event {
+                                match event {
+                                    esp_radio::wifi::ap::EventInfo::Connected(info) => {
+                                        println!("Station connected: {:?}", info);
+                                    }
+                                    esp_radio::wifi::ap::EventInfo::Disconnected(info) => {
+                                        println!("Station disconnected: {:?}", info);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+#[embassy_executor::task(pool_size = 2)]
+async fn net_task(mut runner: Runner<'static, Interface>) {
+    runner.run().await
 }
